59117e44a51681a2e1c57f6a0e3a964e9514f6b4
Yousof Ali <yousofali293@gmail.com>
2024-06-06 11:33:43 -0700

    project setpu done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic molestias asperiores dolorem, laudantium iusto dolorum repellat earum aliquam perspiciatis maiores consequatur numquam alias ratione corrupti architecto, inventore quasi corporis accusantium?</p>
+    </>
+  )
+}
+
+export default App
